2ba7b336f5c1033139f18f7d2e9841b34e132fe2
Nico Hickman <nicohman@protonmail.com>
2019-01-13 17:45:38 -0800

    Add support for multiple domains

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ var uptime = moment.duration(os.uptime(), "seconds");
 console.log(("Current uptime: "+uptime.humanize()).yellow);
 var processes = ['mongod', 'app.js 0', 'app.js 1', 'app.js 2', 'demenses.js'];
 var niceP = ['MongoDB', 'Dragon', 'Defiant', 'Dragon\'s Teeth', 'Demenses']
-console.log("Service Status:");
-processes.forEach(function(pro, ind){
-	var count = exec("ps aux | grep '"+pro+"'").toString().split("\n").length -2;
-	if(count > 0){
-		console.log("\t"+niceP[ind]+" up".green);
-	} else {
-		console.log("\t"+niceP[ind]+ " down".red);
-	}
+var downloads = require("/home/nicohman/ravenserver/downloads.json");
+var domains = ["nicohman.demenses.net", "demenses.net"];
+domains.forEach(function(domain){
+var expT = exec("openssl s_client -connect "+domain+":443 < /dev/null 2>/dev/null | openssl x509 -noout -enddate").toString().replace("notAfter=","").replace(" GMT","");
+var expM = moment(expT, "MMM D HH:mm:ss YYYY").utcOffset(0);
+console.log(domain+" SSL certificate:\n\tExpires "+moment().to(expM).red);
+
 });
-var expT = exec("openssl s_client -connect demenses.net:443 < /dev/null 2>/dev/null | openssl x509 -noout -enddate").toString().replace("notAfter=","").replace(" GMT","");
-var expM = moment(expT, "MMM  D HH:mm:ss 2018").utcOffset(0);
-console.log("SSL certificate:\n\tExpires "+moment().to(expM).red);
 //Call hddtemp and diskspace scripts. Not written by me, but taken from https github.com/Heholord/FalconStats
 //console.log(exec("node /home/nicohman/demstats/hddtemp.js").toString());
 console.log(exec("node "+__dirname+"/diskspace.js").toString());
+console.log("Downloads:");
+Object.keys(downloads).forEach(function(k){
+	console.log(k+" - "+ downloads[k]);
+});
